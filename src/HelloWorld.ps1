7ef8cf637394b04cf2ea1b06216998a1a9c1ecce
Des Finkenzeller <nutsoft@hotmail.com>
2017-03-30 10:44:08 +0100

    Implemented Get-Details

--- src/HelloWorld.ps1
+++ src/HelloWorld.ps1
@@ -5,3 +5,7 @@
 function Get-TextFileNames() {
     Get-ChildItem | Where-Object Name -like *.txt | Select-Object -ExpandProperty Name
 }
+
+function Get-Details {
+    return $env:USERDOMAIN, $env:USERNAME -join '\'
+}
